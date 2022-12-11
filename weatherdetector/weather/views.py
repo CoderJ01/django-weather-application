d90684fe85b192a96a600cc5465b5fac98facae4
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 12:47:49 -0800

    add data

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -8,6 +8,14 @@ def index(request):
         city = request.POST['city']
         res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=3f2e409528a52056b62087fa623591ee').read() # send request to Open Weather API
         json_data = json.loads(res) # retrieve data after request is sent
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' +
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main'['temp']]) + 'k',
+            "pressure": str(json_data['main']['presure']),
+            "humidity": str(json_data['main']['presure']),
+        }
     else:
         city = ''
     return render(request, 'index.html', {'city': city}) # request for index.html file in templates
