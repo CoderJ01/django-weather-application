082ef3d5585d736efa04ad20ee0117a1f49499b4
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 13:22:22 -0800

    runserver (fix error)

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -6,7 +6,7 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=3f2e409528a52056b62087fa623591ee').read() # send request to Open Weather API
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=london&appid=3f2e409528a52056b62087fa623591ee').read() # send request to Open Weather API
         json_data = json.loads(res) # retrieve data after request is sent
         data = {
             "country_code": str(json_data['sys']['country']),
