5d046e3f702aea7b47869592bc2cf6692cd81965
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 11:20:16 -0800

    pseudocode

--- weatherdetector/weather/urls.py
+++ weatherdetector/weather/urls.py
@@ -5,4 +5,5 @@ from . import views # import everything from the views.py file
 urlpatterns = [
     path('', views.index, name="index") # homepage will be left blank, hence the blank single quotes
                                         # views.index means "go to the index function in views.py"
+                                        # the name variable holds the name given to the url
 ]
