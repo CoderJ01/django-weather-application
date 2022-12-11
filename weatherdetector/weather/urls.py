1296edd4fbfce2709679de18213c7f6c204b58a4
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 11:13:52 -0800

    pseudocode

--- weatherdetector/weather/urls.py
+++ weatherdetector/weather/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path # allows for the specification of different urls
-from . import views
+from . import views # import everything from the views.py file
 
 urlpatterns = [
     path('', views.index, name="index")
