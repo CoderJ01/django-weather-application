ac9ca00bdbc21e5c277fc604f0b6ae4aaab4d9e0
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 12:41:14 -0800

    pseudocode

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -7,7 +7,7 @@ def index(request):
     if request.method == 'POST':
         city = request.POST['city']
         res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=3f2e409528a52056b62087fa623591ee').read() # send request to Open Weather API
-        json_data = json.loads(res)
+        json_data = json.loads(res) # retrieve data after request is sent
     else:
         city = ''
     return render(request, 'index.html', {'city': city}) # request for index.html file in templates
